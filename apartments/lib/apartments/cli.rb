7ba4fba3a08e5cf104b4278793a8bf000442e421
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-26 18:12:47 +0000

    modified file

--- apartments/lib/apartments/cli.rb
+++ apartments/lib/apartments/cli.rb
@@ -1,18 +1,38 @@
-class apartments::CLI 
+class Apartments::CLI 
   
   def call
     list_apartments
     menu
+    goodbye
   end 
   
   def list_apartments
     puts "Current Apartments:"
-    puts
-    " 1. Studio in Manhattan - $3000
-      2. Studio in Brooklyn - $2000"
+    puts <<-DOC.gsub /^\s*/, ''
+     1. Studio in Manhattan - $3000
+     2. Studio in Brooklyn - $2000
+     DOC
   end 
   
   def menu
-    puts "Enter the number of bedrooms you are looking for:"
+    input = nil 
+    while input !="exit"
+    puts "Enter the neighbourhood you are looking for or type list to see apartments again or type exit to enter:"
+    input = gets.strip
+       case input
+       when "Manhattan"
+        puts "More apartments in Manhattan..."
+       when "Brooklyn"
+        puts "More apartments in Brooklyn..."
+        when "list"
+          list_apartments
+        else 
+          puts "No results found, type list or exit."
+      end 
+    end 
+  end
+  
+  def goodbye 
+   puts "Thank you for choosing our search engine!"
   end
 end 
