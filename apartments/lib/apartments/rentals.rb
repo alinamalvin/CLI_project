f64909c9b40be8046cfabdc6e438ecf97cdafe16
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-30 21:01:13 +0000

    modified file

--- apartments/lib/apartments/rentals.rb
+++ apartments/lib/apartments/rentals.rb
@@ -10,37 +10,40 @@ def self.scrape_rentals
   rentals=[]
   
   rentals << self.scrape_rentinmanhattan
-
-  # Go to rentinmanhattan, find apartmentshttps://www.rentinmanhattan.net/
-  # extract the properties
-  # instantiate a rental 
-  
-  # Go to streeteasy.. 
+  rentals << self.scrape_streeteasy
+ 
+ rentals
+ end 
+ 
    rental_1 = self.new 
-    rental_1.name = "One bedroom in UWS"
+    rental_1.name = "Studio in UWS"
     rental_1.price = "$2000"
     rental_1.fee = true 
+    rental_1.url="http://rentinmanhattan.net/upper-east-side/apartment-for-rent/328-east-74th-street-1/1074"
 
     
     rental_2 = self.new 
     rental_2.name = "Two bedroom in UES"
     rental_2.price = "$3000"
     rental_2.fee = false
-    
-    
-    rentals
-  end 
+    rental_2.url="htps://streeteasy.com/building/32-west-86-street-new_york/41"
   
     def self.scrape_rentinmanhattan 
     doc = Nokogiri::HTML(open("https://www.rentinmanhattan.net/upper-east-side/apartment-for-rent/328-east-74th-street-1/1074"))
     rental = self.new 
-    rental.name = doc.search("h1.property-address-header").text
-    rental.price = doc.search("h2.property-price-header").text
+    rental.name = doc.search("h1.property-address-header").text.strip
+    rental.price = doc.search("h2.property-price-header").text.strip 
     rental.fee = true
     rental 
-
-    
-    #binding.pry -change when working on url 
   end 
-end  
+  
+  def self.scrape_streeteasy
+    doc = Nokogiri::HTML(open("https://streeteasy.com/building/32-west-86-street-new_york/41"))
+    rental = self.new
+    rental.name = doc.search("a.incognito").text
+    rental.price = doc.search("div.price").text
+    rental_fee=false 
+    rental
+end 
 
+end
