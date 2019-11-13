400ab56377b4f6083a8a19e2832966fbec317993
Alina Kornilova <alinakornilova78@gmail.com>
2019-11-13 00:49:38 +0000

    modified file

--- apartments/lib/apartments/cli.rb
+++ apartments/lib/apartments/cli.rb
@@ -7,34 +7,34 @@ class Apartments::CLI
   
   def list_apartments
     puts "Current Apartments:"
-     @rentals = Apartments::Rentals.today
-     @rentals.each.with_index(1) do |rentals,i|
+    @rentals = Apartments::Rentals.today
+    @rentals.each.with_index(1) do |rentals,i|
        puts "#{i} - #{rentals.name} - #{rentals.price} - #{rentals.fee}"
-     end
+    end
   end 
   
   def menu
     input = nil 
-    while input !="exit"
+    while input !="exit" do
       puts "Enter the amount of bedroom you are looking for or type list to see apartments again or type exit to enter:"
-    input = gets.strip
+      input = gets.strip
     
-    if input.to_i > 0 && input.to_i < 3
-      the_rental = @rentals[input.to_i-1]
-      puts "#{the_rental.name} - #{the_rental.price} - #{the_rental.fee}"
-    else if input == "list"
-      list_apartments
-    else if input=="exit"
-       goodbye
-    else 
-       puts "No results found, type list or exit."
-     end 
+      if input.to_i > 0 && input.to_i < @rentals.length + 1
+        the_rental = @rentals[input.to_i-1]
+        puts "#{the_rental.name} - #{the_rental.price} - #{the_rental.fee}"
+      elsif input == "list"
+        list_apartments
+      elsif input=="exit"
+        goodbye
+      else
+        puts "No results found, type list or exit."
+      end
     end
   end 
-end 
-end 
+  
    
   def goodbye 
     puts "Thank you for choosing our search engine!"
   end
+  
 end 
